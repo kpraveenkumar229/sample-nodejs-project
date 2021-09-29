417787b3e440ee3c8a9446df70a32318fd1ff3b3
praveen kumar <81840707+kpraveenkumar229@users.noreply.github.com>
2021-09-29 13:34:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Helloo Testing DevOps Node! JS Project Hi Hello  How are you ? welcome to devOps Project')
+  response.send('Helloo Testing DevOps Node! JS Project Hi Hello  How are you ? welcome to devOpss Project')
 }) 
 
 app.listen(app.get('port'), function() {
